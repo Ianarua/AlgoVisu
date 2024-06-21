a85003aca910c66729ab0d0e17f4211f92389bb8
Ianarua <3067815232@qq.com>
2024-06-21 23:25:49 +0800

    feat: 添加装饰器支持

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,20 +1,22 @@
-import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
+// 支持装饰器
+import { swcPlugin } from 'electron-vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+    main: {
+        plugins: [externalizeDepsPlugin(), swcPlugin()]
     },
-    plugins: [react()]
-  }
-})
+    preload: {
+        plugins: [externalizeDepsPlugin()]
+    },
+    renderer: {
+        resolve: {
+            alias: {
+                '@renderer': resolve('src/renderer/src')
+            }
+        },
+        plugins: [react()]
+    }
+});
