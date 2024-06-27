65bd22ae8adaef6d7a640110ca290f3791ab9b51
Ianarua <3067815232@qq.com>
2024-06-27 12:49:15 +0800

    fix main.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,7 +2,7 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron';
 import { join } from 'path';
 import { electronApp, optimizer, is } from '@electron-toolkit/utils';
 // import icon from '../../resources/icon.png?asset';
-import icon from '../../resources/icon.png?asset';
+const icon = require('../../resources/icon.png')
 
 function createWindow (): void {
     // Create the browser window.
