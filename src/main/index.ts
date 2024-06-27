0663ea26703aac43ac5d962d3f9ffa06b6741354
Ianarua <3067815232@qq.com>
2024-06-27 14:58:26 +0800

    feat: hanoitower disk color

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,7 +2,8 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron';
 import { join } from 'path';
 import { electronApp, optimizer, is } from '@electron-toolkit/utils';
 // import icon from '../../resources/icon.png?asset';
-const icon = require('../../resources/icon.png')
+
+// const icon = require('../../resources/icon.png')
 
 function createWindow (): void {
     // Create the browser window.
@@ -11,7 +12,7 @@ function createWindow (): void {
         height: 670,
         show: false,
         autoHideMenuBar: true,
-        ...(process.platform === 'linux' ? { icon } : {}),
+        ...(process.platform === 'linux' ? { icon: '../../resources/icon.png' } : {}),
         webPreferences: {
             preload: join(__dirname, '../preload/index.js'),
             sandbox: false
